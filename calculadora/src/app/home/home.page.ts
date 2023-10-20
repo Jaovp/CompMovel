7c04ed48f158f3f7602af92548fd3cc1a9fcb42d
Jaovp <victorpeet1@gmail.com>
2023-10-20 10:38:44 -0300

    operações

--- calculadora/src/app/home/home.page.ts
+++ calculadora/src/app/home/home.page.ts
@@ -6,8 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  visor : number = 0;
+  visor : string = '0';
+  operacao! : number;
+  valor1! : number;
+  valor2! : number;
 
   constructor() {}
+
+  adicionarNumero(valor : string) {
+    if((this.visor.length === 1) && (this.visor === '0')) {
+      this.visor = valor;
+    }else{
+      this.visor += valor;
+    }
+  }
+
+  deletar() {
+    this.visor = '0';
+  }
+
+  adicionarOperacao(valor : number){
+    this.operacao = valor;
+    this.valor1 = +this.visor;
+    console.log(this.valor1);
+    
+  }
+
+  calcular() {
+    this.valor2 = +this.visor;
+    switch(this.operacao) {
+      case 0: {
+        this.visor = "" + (this.valor1 + this.valor2);
+        break;
+      }
+      case 1: {
+        this.visor = "" + (this.valor1 - this.valor2);
+        break;
+      }
+      case 2: {
+        this.visor = "" + (this.valor1 * this.valor2);
+        break;
+      }
+      case 3: {
+        this.visor = "" + (this.valor1 / this.valor2);
+        break;
+      }
+      case 4: {
+        this.visor = "" + (this.valor1 / 100);
+        break;
+      }
+      case 5: {
+        this.visor = "" + (-this.valor1);
+      }
+      }
+
+    }
+  
 
 }
